6e8eeb0023037548a943074d5e994f084f6812e2
Juan <rigualjuan@gmail.com>
2023-03-06 20:03:29 -0300

    feat: add strict typing to memoizer

--- src/memoizer/memoizer.ts
+++ src/memoizer/memoizer.ts
@@ -1,10 +1,14 @@
-export const memoizer = (callback) => {
-  const cache = new Map();
-  return (...args) => {
+import { Callback } from "../types";
+
+export const memoizer = <T>(callback: Callback<T>) => {
+  const cache = new Map<string, ReturnType<Callback<T>>>();
+
+  return (...args: T[]): ReturnType<Callback<T>> => {
     const key = args.toString();
     const hasKey = cache.has(key);
+
     if (!hasKey) {
-      const resultFromCallback = callback(...args);
+      const resultFromCallback = callback(...args) as ReturnType<Callback<T>>;
       cache.set(key, resultFromCallback);
       return resultFromCallback;
     }
