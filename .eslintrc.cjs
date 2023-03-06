000b320f4b96856de833b30bc319dda8fbfcd47e
Juan <rigualjuan@gmail.com>
2023-03-06 15:39:07 -0300

    chore: add eslint and pre-commit hook

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    "no-var": "error",
+    "prefer-const": "error",
+  },
+};
