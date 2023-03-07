25b21e20c07bb8cf58ebe53978ebe8179bfd913e
Juan <rigualjuan@gmail.com>
2023-03-07 17:10:12 -0300

    fix: remove testing code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import { memoizer } from "./memoizer/memoizer";
-
-const sumaYResta = (a: number, b: number) => {
-  for (let i = 0; i < 9_000_000_000; i++) {}
-  return a + b;
-};
-
-const memoizedSumaYResta = memoizer(sumaYResta);
-
-console.log("result slow ", memoizedSumaYResta(1, 2));
-console.log("result cache ", memoizedSumaYResta(1, 2));
+export * from "./memoizer";
